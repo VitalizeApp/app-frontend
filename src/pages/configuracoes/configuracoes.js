fe82c0b0e11d56a573e0c41ce437b967b3a16320
Gian Marco <gian.crelikc@ba.estudante.senai.br>
2025-05-26 13:51:05 +0000

    Modo escuro funcional
    Modo escuro se estende agora para outras páginas ao ativar nas configurações
    No entanto está em teste por enquanto, é preciso definir a cor de cada elemento individualmente no css e aplicar o js do configurações a todas as páginas "/src/pages/configuracoes/configuracoes.js".
    Únicas páginas com modo escuro são:
    configuracoes.html
    home.html
    lembretes.html

--- src/pages/configuracoes/configuracoes.js
+++ src/pages/configuracoes/configuracoes.js
@@ -1,6 +1,23 @@
 
+//Essa função pode ser usada para mudar em todas as páginas, apenas conecte esse JS a todas as telas e defina um modo escuro para os elementos no CSS
 
-// Função para alternar modo escuro
-function alterarModo() {
-    document.body.classList.toggle("dark-mode");
+let html = document.querySelector ("html");
+
+function tema() {
+    let tema = html.getAttribute("data-theme");
+
+    if (tema === "light") {
+        html.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }else{
+        html.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let temaSalvo = localStorage.getItem("theme");
+    if (temaSalvo) {
+        html.setAttribute("data-theme", temaSalvo)
+    }
+})
